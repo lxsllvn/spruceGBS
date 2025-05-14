e1da77c0e6a6f6536213dd2cca4639266316880b
Alexis Sullivan <lxsllvn@b-an01.hpc2n.umu.se>
2025-05-14 22:52:40 +0200

    Remove paths specific to my setup

--- 04_realignment/GATK_indelrealigner.sh
+++ 04_realignment/GATK_indelrealigner.sh
@@ -14,17 +14,12 @@ if [ -z "$SAMPLE" ]; then
 fi
 
 # Set paths
-GATK="/pfs/proj/nobackup/fs/projnb10/wanglab-2020/Alex/alignments"
 REF="${SPRUCE_PROJECT}/ref/picea_newref.fa"
 INPUT="${SPRUCE_PROJECT}/bams/intersected"
 TARGETS="${SPRUCE_PROJECT}/bams/realigned/realign_intervals"
 
 # Set output directory
 OUTDIR="${SPRUCE_PROJECT}/bams/realigned"
-
-# Use local Java 8 install for compatibility with GATK 3.7
-export JAVA_HOME="/pfs/proj/nobackup/fs/projnb10/wanglab-2020/Alex/java/jdk8u392-b08"
-export PATH="$JAVA_HOME/bin:$PATH"
 
 # Load required modules
 ml GCC/13.2.0
